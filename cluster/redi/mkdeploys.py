a561569f3cf3ae322c31cafc26aa9e9625bb88c0
Jonathan Guiang <jkguiang@gmail.com>
2021-12-23 10:15:30 -0800

    modified to generate certs

--- cluster/redi/mkdeploys.py
+++ cluster/redi/mkdeploys.py
@@ -1,4 +1,5 @@
 import sys, os
+from subprocess import Popen, PIPE
 sys.path.append(os.getcwd()+"/..")
 from utils import DeploymentWriter
 
@@ -6,7 +7,7 @@ class RediDeploymentWriter(DeploymentWriter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def replace_placeholders(self, config, name, text):
+    def _replace_placeholders(self, config, name, text):
         text = text.replace("NODE_PLACEHOLDER", config["node"])
         text = text.replace("NAME_PLACEHOLDER", name)
         text = text.replace("INTF_PLACEHOLDER", config["interface"])
@@ -14,6 +15,15 @@ class RediDeploymentWriter(DeploymentWriter):
         text = text.replace("REDI_PORT_PLACEHOLDER", config["redi_port"])
         text = text.replace("VLAN_PLACEHOLDER", config["vlan"])
         return text
+
+    def make_certs(self):
+        if len(self._get_deployments()) == 0:
+            print("WARNING: No certs made; run write() first!")
+            return
+        for config in self.configs:
+            base_dir = f"{os.getcwd()}/{self.deployment_dir}/{self._get_deployment_name(config)}"
+            cmd = f"../certs/generate.sh {config['vlan']} {base_dir}"
+            Popen(cmd.split(), cwd="../certs", stdout=PIPE).communicate()
 
 if __name__ == "__main__":
     redi_configs = [
@@ -33,9 +43,10 @@ if __name__ == "__main__":
         }, 
     ]
     deployment_writer = RediDeploymentWriter(
-        base_dir="redis", 
+        base_dir="./", 
         template_dir="templates", 
         app_name="rucio-sense-redi", 
         configs=redi_configs
     )
     deployment_writer.write()
+    deployment_writer.make_certs()
