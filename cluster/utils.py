317f699c26359a5e3ba75786381ae1c3911e722c
Jonathan Guiang <jkguiang@gmail.com>
2021-12-23 10:19:22 -0800

    moved most 'private' methods to 'protected'

--- cluster/utils.py
+++ cluster/utils.py
@@ -12,61 +12,64 @@ class DeploymentWriter:
 
     def add_config(self, new_config):
         self.configs.append(new_config)
-        self.__check_configs()
+        self._check_configs()
 
     def write(self):
-        self.__check_configs()
+        self._check_configs()
         os.makedirs(self.base_dir, exist_ok=True)
-        for old_deployment in self.__get_deployments():
+        for old_deployment in self._get_deployments():
             for f in glob.glob(f"{old_deployment}/*"):
                 os.remove(f)
             os.rmdir(old_deployment)
 
         for config in self.configs:
-            N = config["node"].split(".")[0].split("-")[-1]
-            name = f"{self.app_name}-{N}-{config['interface'].replace('.', '-')}"
-            self.__write_deployment(config, name)
+            name = self._get_deployment_name(config)
+            self._write_deployment(config, name)
 
         with open(f"{self.base_dir}/Makefile", "w") as f_out:
             f_out.write("delete:\n")
-            for new_deployment in self.__get_deployments():
+            for new_deployment in self._get_deployments():
                 local_path = new_deployment.replace(f"{self.base_dir}/", "")
                 delete_cmd = f"\t- kubectl delete -k ./{local_path}"
                 if self.namespace != "":
                     delete_cmd += f" -n {self.namespace}"
                 f_out.write(f"{delete_cmd}\n")
             f_out.write("create:\n")
-            for new_deployment in self.__get_deployments():
+            for new_deployment in self._get_deployments():
                 local_path = new_deployment.replace(f"{self.base_dir}/", "")
                 apply_cmd = f"\t- kubectl apply -k ./{local_path}"
                 if self.namespace != "":
                     apply_cmd += f" -n {self.namespace}"
                 f_out.write(f"{apply_cmd}\n")
 
-    def strip_comments(self, extension, text):
+    def _get_deployment_name(self, config):
+        N = config["node"].split(".")[0].split("-")[-1]
+        return f"{self.app_name}-{N}-{config['interface'].replace('.', '-')}"
+
+    def _strip_comments(self, extension, text):
         if extension in ("yaml", "cfg"):
             return "\n".join([l for l in text.split("\n") if l.strip()[:1] != "#"])
         else:
             return text
 
-    def replace_placeholders(self, config, name, text):
+    def _replace_placeholders(self, config, name, text):
         raise NotImplementedError
 
-    def __check_configs(self):
+    def _check_configs(self):
         if len(self.configs) > 1:
             for i, cfg in enumerate(self.configs[:-1]):
                 assert cfg.keys() == self.configs[i+1].keys()
 
-    def __get_deployments(self):
+    def _get_deployments(self):
         return [d for d in glob.glob(f"{self.deployment_dir}/*")]
 
-    def __write_deployment(self, config, name):
+    def _write_deployment(self, config, name):
         os.makedirs(f"{self.deployment_dir}/{name}", exist_ok=True)
         for template in glob.glob(f"{self.template_dir}/*"):
             with open(template, "r") as f_in:
                 text = f_in.read()
-                text = self.strip_comments(template.split(".")[-1], text)
-                text = self.replace_placeholders(config, name, text)
+                text = self._strip_comments(template.split(".")[-1], text)
+                text = self._replace_placeholders(config, name, text)
             with open(f"{self.deployment_dir}/{name}/{template.split('/')[-1]}", "w") as f_out:
                 f_out.write(text)
 
