ddce890ebfbb0618d40141b2f4ae81437da60fb4
Jonathan Guiang <jkguiang@gmail.com>
2022-01-06 15:23:23 -0800

    probably meaningless change

--- cluster/certs/generate.sh
+++ cluster/certs/generate.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 export PASSPHRASE=123456
 
-HOSTNAME=$1
+HOSTNAME=$1 # IPv6 address (no brackets, no capitals)
 BASEDIR=$2
 
-# XrootD Server 1
 openssl req -new -newkey rsa:2048 -nodes \
     -keyout $BASEDIR/hostcert.key.pem -subj "/CN=$HOSTNAME" > $BASEDIR/hostcert.csr
 openssl x509 -req -days 9999 -CAcreateserial \
-    -extfile <(printf "subjectAltName=IP:$HOSTNAME") \
+    -extfile <(printf "subjectAltName=DNS:$HOSTNAME,IP:$HOSTNAME") \
     -in $BASEDIR/hostcert.csr \
     -CA rucio_ca.pem -CAkey rucio_ca.key.pem \
     -out $BASEDIR/hostcert.pem \
