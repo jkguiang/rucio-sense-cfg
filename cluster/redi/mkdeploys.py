2e61538ed6b7104cdcf9db1f8087bbe83dfc55a7
Jonathan Guiang <jkguiang@gmail.com>
2022-01-21 14:22:01 -0800

    fixed ipv6s, changed sitenames, added test files

--- cluster/redi/mkdeploys.py
+++ cluster/redi/mkdeploys.py
@@ -8,17 +8,22 @@ class RediDeploymentWriter(DeploymentWriter):
         super().__init__(*args, **kwargs)
 
     def _replace_placeholders(self, config, name, text):
+        text = text.replace("SITE_PLACEHOLDER", self._get_site_name(config))
         text = text.replace("NODE_PLACEHOLDER", config["node"])
         text = text.replace("NAME_PLACEHOLDER", name)
         text = text.replace("INTF_PLACEHOLDER", config["interface"])
         text = text.replace("MAIN_PORT_PLACEHOLDER", config["main_port"])
         text = text.replace("REDI_PORT_PLACEHOLDER", config["redi_port"])
-        text = text.replace("VLAN_PLACEHOLDER", config["vlan"])
+        text = text.replace("IPV6_PLACEHOLDER", config["ipv6"])
         return text
 
     def _get_deployment_name(self, config):
         N = config["node"].split(".")[0].split("-")[-1]
         return f"{self.app_name}-{N}-{config['main_port'].replace('.', '-')}"
+
+    def _get_site_name(self, config):
+        ipv6_last4 = config["ipv6"].split(":")[-1]
+        return f"RUCIO_SENSE_REDI_{ipv6_last4}_{config['main_port']}_{config['redi_port']}"
 
     def make_certs(self):
         if len(self._get_deployments()) == 0:
@@ -26,24 +31,31 @@ class RediDeploymentWriter(DeploymentWriter):
             return
         for config in self.configs:
             base_dir = f"{os.getcwd()}/{self.deployment_dir}/{self._get_deployment_name(config)}"
-            cmd = f"../certs/generate.sh {config['vlan']} {base_dir}"
+            cmd = f"../certs/generate.sh {config['ipv6']} {base_dir}"
             Popen(cmd.split(), cwd="../certs", stdout=PIPE).communicate()
 
 if __name__ == "__main__":
     redi_configs = [
         {
-            "node": "nrp-02.nrp-nautilus.io", 
+            "node": "nrp-01.nrp-nautilus.io", 
             "main_port": "2094",
             "redi_port": "9001",
-            "interface": "enp2s0f1",
-            "vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c04f",
+            "interface": "macvlan0",
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c171",
         }, 
         {
-            "node": "nrp-02.nrp-nautilus.io", 
+            "node": "nrp-01.nrp-nautilus.io", 
             "main_port": "2095",
             "redi_port": "9002",
-            "interface": "enp2s0f1",
-            "vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c050",
+            "interface": "macvlan1",
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c172",
+        }, 
+        {
+            "node": "nrp-01.nrp-nautilus.io", 
+            "main_port": "2096",
+            "redi_port": "9003",
+            "interface": "macvlan2",
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c173",
         }, 
     ]
     deployment_writer = RediDeploymentWriter(
--- cluster/server/mkdeploys.py
+++ cluster/server/mkdeploys.py
@@ -8,18 +8,23 @@ class ServerDeploymentWriter(DeploymentWriter):
         super().__init__(*args, **kwargs)
 
     def _replace_placeholders(self, config, name, text):
+        text = text.replace("SITE_PLACEHOLDER", self._get_site_name(config))
         text = text.replace("NODE_PLACEHOLDER", config["node"])
         text = text.replace("NAME_PLACEHOLDER", name)
-        text = text.replace("REDI_VLAN_PLACEHOLDER", config["redi_vlan"])
+        text = text.replace("REDI_IPV6_PLACEHOLDER", config["redi_ipv6"])
         text = text.replace("REDI_PORT_PLACEHOLDER", config["redi_port"])
         text = text.replace("INTF_PLACEHOLDER", config["interface"])
         text = text.replace("PORT_PLACEHOLDER", config["port"])
-        text = text.replace("VLAN_PLACEHOLDER", config["vlan"])
+        text = text.replace("IPV6_PLACEHOLDER", config["ipv6"])
         return text
 
     def _get_deployment_name(self, config):
         N = config["node"].split(".")[0].split("-")[-1]
         return f"{self.app_name}-{N}-{config['port'].replace('.', '-')}"
+
+    def _get_site_name(self, config):
+        ipv6_last4 = config["ipv6"].split(":")[-1]
+        return f"RUCIO_SENSE_SERVER_{ipv6_last4}_{config['port']}"
 
     def make_certs(self):
         if len(self._get_deployments()) == 0:
@@ -27,26 +32,34 @@ class ServerDeploymentWriter(DeploymentWriter):
             return
         for config in self.configs:
             base_dir = f"{os.getcwd()}/{self.deployment_dir}/{self._get_deployment_name(config)}"
-            cmd = f"../certs/generate.sh {config['vlan']} {base_dir}"
+            cmd = f"../certs/generate.sh {config['ipv6']} {base_dir}"
             Popen(cmd.split(), cwd="../certs", stdout=PIPE).communicate()
 
 if __name__ == "__main__":
     server_configs = [
         {
-            "node": "nrp-01.nrp-nautilus.io", 
-            "vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c171",
+            "node": "nrp-02.nrp-nautilus.io", 
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c050",
             "port": "2094",
-            "interface": "enp2s0f1",
-            "redi_vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c04f",
+            "interface": "macvlan0",
+            "redi_ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c171",
             "redi_port": "9001"
         }, 
         {
-            "node": "nrp-01.nrp-nautilus.io", 
-            "vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c173",
+            "node": "nrp-02.nrp-nautilus.io", 
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c051",
             "port": "2095",
-            "interface": "enp2s0f1",
-            "redi_vlan": "2607:f720:1720:e00e:ec4:7aff:febb:c050",
+            "interface": "macvlan1",
+            "redi_ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c172",
             "redi_port": "9002"
+        }, 
+        {
+            "node": "nrp-02.nrp-nautilus.io", 
+            "ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c052",
+            "port": "2096",
+            "interface": "macvlan2",
+            "redi_ipv6": "2607:f720:1720:e00e:ec4:7aff:febb:c173",
+            "redi_port": "9003"
         }, 
     ]
     deployment_writer = ServerDeploymentWriter(
