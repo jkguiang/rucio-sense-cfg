b26a94a778f5127d40d993f5d2e34ea84385f48e
aaarora <aaarora@ucsd.edu>
2023-03-07 18:28:42 -0800

    pass num files as arg

--- cluster/docker/setup.sh
+++ cluster/docker/setup.sh
@@ -3,7 +3,7 @@
 ip -6 route del default via fe80::ecee:eeff:feee:eeee
 
 mkdir -p /rucio/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1/50000/
-for i in {0..40}; do 
+for i in {0..$1}; do 
   dd if=/dev/zero of=/rucio/testSourceFile$i bs=1G count=1;
   ln /rucio/testSourceFile$i /rucio/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1/50000/testSourceFile{$i}.root  
 done
